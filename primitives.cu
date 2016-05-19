1c5f5be34da449008a0ecaf70f627eb00364e2cb
Brian Tarasinski <brianzi@physik.fu-berlin.de>
2016-05-19 16:12:57 +0200

    fix kernels to use floats only

--- primitives.cu
+++ primitives.cu
@@ -2,7 +2,7 @@
 #include <cuComplex.h>
 
 // the block size is 1<<LOG_BK_SIZE in x and y direction
-#define LOG_BK_SIZE = 5 
+#define LOG_BK_SIZE (5)
 
 
 
@@ -10,8 +10,7 @@
 //partake in the cphase.
 //no shared memory needed
 //run in a 2d grid that stretches over dm10
-__global__ void cphase(cuFloatComplex *dm10, unsigned int mask) {
-
+__global__ void cphase(float *dm10, unsigned int mask) {
     const int x = (blockIdx.x << LOG_BK_SIZE) + threadIdx.x;
     const int y = (blockIdx.y << LOG_BK_SIZE) + threadIdx.y;
 
@@ -27,7 +26,7 @@ __global__ void cphase(cuFloatComplex *dm10, unsigned int mask) {
 //mask must have exactly one bit flipped, denoting which byte is involved
 //the results is multiplied with mul, to obtain unitary, set mul = 1/sqrt(2)
 
-__global__ void hadamard(cuFloatComplex *dm10, unsigned int mask, float mul) { 
+__global__ void hadamard(float *dm10, unsigned int mask, float mul) { 
 
     const int x = (blockIdx.x << LOG_BK_SIZE) + threadIdx.x;
     const int y = (blockIdx.y << LOG_BK_SIZE) + threadIdx.y;
@@ -35,42 +34,32 @@ __global__ void hadamard(cuFloatComplex *dm10, unsigned int mask, float mul) {
     if (x>y) return;
     if ((x & mask) || (y & mask)) return;
 
-    float *a_re = &dm10[ ((x & ~qmask) << 10) + (y&~qmask) ];
-    float *a_im = &dm10[ (x & ~qmask) + ((y&~qmask) << 10) ];
 
-    float *b_re = &dm10[ ((x &~qmask) << 10) + (y|qmask) ];
-    float *b_im = &dm10[ (x &~qmask) + ((y|qmask) << 10) ];
+    //TODO: optimize to only read once and write once...
 
-    float *c_re = &dm10[ ((x | qmask) << 10) + (y | qmask) ];
-    float *a_im = &dm10[ (x  | qmask) + ((y |qmask) << 10) ];
+    float *a_re = &dm10[ (x << 10) + y ];
+    float *a_im = &dm10[ x + (y << 10) ];
+
+    //note that y|mask >= y, so we know which one is the real part
+    float *b_re = &dm10[ (x << 10) + (y | mask) ];
+    float *b_im = &dm10[ x + ((y | mask) << 10) ];
+
+    float *c_re = &dm10[ ((x | mask) << 10) + (y | mask) ];
+    float *c_im = &dm10[ (x | mask) + ((y | mask) << 10) ];
 
 
-    //this is most probably very bad, and
-    //we should reconsider having simply one thread per 2x2 matmul.
-
-    //we are writing the a_block
-    //if (!(x & mask) && !(y & mask)) {
-        float new_a_re = &a_re + &c_re + 2*&b_re;
-        float new_a_im = &a_im + &c_im;
-        *a_re = mul*new_a_re;
-        *a_im = mul*new_a_im;
-    //}
-
-    //we are writing the b block
-    //if (!(x & mask) && (y & mask)) {
-        float new_b_re = a_re - c_re;
-        float new_b_im = a_im - c_im - 2*b_im;
-        *b_re = mul*new_b_re;
-        *b_im = mul*new_b_im;
-    //}
-
-    //we are writing the c block
-    //if((x & mask) && (y & mask)) {
-        float new_c_re = a_re + c_re - 2*b_re;
-        float new_c_im = a_im + c_im;
-        *c_re = mul*new_c_re;
-        *c_im = mul*new_c_im;
-    //}
+    float new_a_re = *a_re + *c_re + 2*(*b_re);
+    float new_a_im = *a_im + *c_im;
+    *a_re = mul*new_a_re;
+    *a_im = mul*new_a_im;
+    float new_b_re = *a_re - *c_re;
+    float new_b_im = *a_im - *c_im - 2*(*b_im);
+    *b_re = mul*new_b_re;
+    *b_im = mul*new_b_im;
+    float new_c_re = *a_re + *c_re - 2*(*b_re);
+    float new_c_im = *a_im + *c_im;
+    *c_re = mul*new_c_re;
+    *c_im = mul*new_c_im;
 }
 
 
