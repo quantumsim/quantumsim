b345153e06deaf126932813f430dcd01b0abf304
Brian Tarasinski <brianzi@physik.fu-berlin.de>
2016-05-24 20:00:47 +0200

    working showcase implementation of threeqbit

--- threeqb.py
+++ threeqb.py
@@ -1,5 +1,5 @@
 import sparsedm
-
+import numpy as np
 
 sdm = sparsedm.SparseDM(5)
 
@@ -10,7 +10,7 @@ sdm.classical[2] = 0
 
 print(sdm.classical)
 
-for t in range(10):
+for t in range(20):
     sdm.hadamard(4) 
     sdm.cphase(4, 2)
     sdm.cphase(4, 1)
@@ -18,9 +18,10 @@ for t in range(10):
 
     sdm.amp_ph_damping(4, 0.1, 0)
 
-    # max likelihood sampling
+    # natural sampling
+    r = np.random.random()
     p0, p1 = sdm.peak_measurement(4)
-    if p0 > p1:
+    if p0/(p0 + p1) > r:
         sdm.project_measurement(4, 0)
     else:
         sdm.project_measurement(4, 1)
@@ -29,9 +30,11 @@ for t in range(10):
     sdm.cphase(3, 1)
     sdm.cphase(3, 0)
     sdm.hadamard(3)
+    sdm.amp_ph_damping(3, 0.1, 0)
 
+    r = np.random.random()
     p0, p1 = sdm.peak_measurement(3)
-    if p0 > p1:
+    if p0/(p0 + p1) > r:
         sdm.project_measurement(3, 0)
     else:
         sdm.project_measurement(3, 1)
