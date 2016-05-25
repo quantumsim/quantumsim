faf5bd53e44f5f3517b8644c748c4c2a907678cf
Brian Tarasinski <brianzi@physik.fu-berlin.de>
2016-05-25 14:26:05 +0200

    name qubits in threeqb.py

--- threeqb.py
+++ threeqb.py
@@ -1,42 +1,42 @@
 import sparsedm
 import numpy as np
 
-sdm = sparsedm.SparseDM(5)
+sdm = sparsedm.SparseDM(["D1", "D2", "D3", "A1", "A2"])
 
 for bit in sdm.classical:
     sdm.classical[bit] = 1
 
-sdm.classical[2] = 0
+sdm.classical["D3"] = 0
 
 print(sdm.classical)
 
 for t in range(20):
-    sdm.hadamard(4) 
-    sdm.cphase(4, 2)
-    sdm.cphase(4, 1)
-    sdm.hadamard(4)
+    sdm.hadamard("A1") 
+    sdm.cphase("A1", "D1")
+    sdm.cphase("A1", "D2")
+    sdm.hadamard("A1")
 
-    sdm.amp_ph_damping(4, 0.1, 0)
+    sdm.amp_ph_damping("A1", 0.1, 0)
 
     # natural sampling
     r = np.random.random()
-    p0, p1 = sdm.peak_measurement(4)
+    p0, p1 = sdm.peak_measurement("A1")
     if p0/(p0 + p1) > r:
-        sdm.project_measurement(4, 0)
+        sdm.project_measurement("A1", 0)
     else:
-        sdm.project_measurement(4, 1)
+        sdm.project_measurement("A1", 1)
 
-    sdm.hadamard(3) 
-    sdm.cphase(3, 1)
-    sdm.cphase(3, 0)
-    sdm.hadamard(3)
-    sdm.amp_ph_damping(3, 0.1, 0)
+    sdm.hadamard("A2") 
+    sdm.cphase("A2", "D2")
+    sdm.cphase("A2", "D3")
+    sdm.hadamard("A2")
+    sdm.amp_ph_damping("A2", 0.1, 0)
 
     r = np.random.random()
-    p0, p1 = sdm.peak_measurement(3)
+    p0, p1 = sdm.peak_measurement("A2")
     if p0/(p0 + p1) > r:
-        sdm.project_measurement(3, 0)
+        sdm.project_measurement("A2", 0)
     else:
-        sdm.project_measurement(3, 1)
+        sdm.project_measurement("A2", 1)
 
     print(sdm.classical, sdm.trace())
