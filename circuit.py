c90acfec94175897ab9ba323b6f0bf44c560601f
Brian Tarasinski <brianzi@physik.fu-berlin.de>
2016-05-30 16:18:47 +0200

    implement and test Circuit.apply_to()

--- circuit.py
+++ circuit.py
@@ -1,6 +1,8 @@
 import matplotlib as mp
 import matplotlib.pyplot as plt
 
+import numpy as np
+
 import tp
 
 
@@ -30,20 +32,22 @@ class Circuit:
                     AmpPhDamp(
                         str(b),
                         (gts[0].time + tmin) / 2,
-                        gts[0].time - tmin))
+                        gts[0].time - tmin,
+                    b.t1, b.t2))
             if abs(tmax - gts[-1].time) > 1e-6:
                 self.add_gate(AmpPhDamp(
-                    str(b), (gts[-1].time + tmax) / 2, tmax - gts[-1].time))
+                    str(b), (gts[-1].time + tmax) / 2, tmax - gts[-1].time,
+                    b.t1, b.t2))
 
             for g1, g2 in zip(gts[:-1], gts[1:]):
                 self.add_gate(
                     AmpPhDamp(
                         str(b),
                         (g1.time + g2.time) / 2,
-                        g2.time - g1.time))
-                print("added waiting on", str(b))
+                        g2.time - g1.time,
+                    b.t1, b.t2))
 
-    def get_order(self):
+    def order(self):
         all_gates = list(enumerate(sorted(self.gates, key=lambda g: g.time)))
         measurements = [n for n, gate in all_gates if gate.is_measurement]
         dependencies = {n: set() for n, gate in all_gates}
@@ -57,6 +61,16 @@ class Circuit:
 
         for n, i in enumerate(order):
             all_gates[i][1].annotation = "%d" % n
+
+        new_order = []
+        for i in order:
+            new_order.append(all_gates[i][1])
+
+        self.gates = new_order
+
+    def apply_to(self, sdm):
+        for gate in self.gates:
+            gate.apply_to(sdm)
 
     def plot(self):
         times = [g.time for g in self.gates]
@@ -107,15 +121,13 @@ class Qubit:
 
     def __init__(self, name, t1, t2):
         self.name = name
-        self.t1 = t1
-        self.t2 = t2
+        self.t1 = max(t1, 1e-10)
+        self.t2 = max(t2, 1e-10)
 
     def __str__(self):
         return self.name
 
-
 class Gate:
-
     def __init__(self, time):
         self.is_measurement = False
         self.time = time
@@ -145,21 +157,26 @@ class Gate:
     def involves_qubit(self, bit):
         return bit in self.involved_qubits
 
+    def apply_to(self, sdm):
+        f = sdm.__getattribute__(self.method_name)
+
+        f(*self.involved_qubits, **self.method_params)
 
 class Hadamard(Gate):
-
     def __init__(self, bit, time):
         super().__init__(time)
         self.involved_qubits.append(bit)
         self.label = r"$H$"
-
+        self.method_name = "hadamard"
+        self.method_params = {}
 
 class CPhase(Gate):
-
     def __init__(self, bit0, bit1, time):
         super().__init__(time)
         self.involved_qubits.append(bit0)
         self.involved_qubits.append(bit1)
+        self.method_name = "cphase"
+        self.method_params = {}
 
     def plot_gate(self, ax, coords):
         bit0 = self.involved_qubits[0]
@@ -172,13 +189,16 @@ class CPhase(Gate):
         line = mp.lines.Line2D(xdata, ydata, color='k')
         ax.add_line(line)
 
-
 class AmpPhDamp(Gate):
-
-    def __init__(self, bit, time, duration):
+    def __init__(self, bit, time, duration, t1, t2):
         super().__init__(time)
         self.involved_qubits.append(bit)
         self.duration = duration
+        self.t1 = t1
+        self.t2 = t2
+        self.method_name = "amp_ph_damping"
+        self.method_params = {"gamma": 1 - np.exp(-duration/t1),
+                "lamda": 1 - np.exp(-duration/t2) }
 
     def plot_gate(self, ax, coords):
         ax.scatter((self.time),
@@ -189,14 +209,30 @@ class AmpPhDamp(Gate):
                 self.involved_qubits[0]]), xytext=(
                 0, 3), textcoords='offset points', ha='center')
 
-
 class Measurement(Gate):
-
-    def __init__(self, bit, time):
+    def __init__(self, bit, time, sampler):
         super().__init__(time)
         self.is_measurement = True
         self.involved_qubits.append(bit)
         self.label = r"$\circ\!\!\!\!\!\!\!\nearrow$"
+
+        self.sampler = sampler
+
+
+        self.measurements = []
+
+
+    def apply_to(self, sdm):
+        bit = self.involved_qubits[0]
+        p0, p1 = sdm.peak_measurement(bit)
+
+        r = np.random.random()
+        if r < p0/(p0+p1):
+            self.measurements.append(0)
+            sdm.project_measurement(bit, 0)
+        else:
+            self.measurements.append(1)
+            sdm.project_measurement(bit, 1)
 
 
 if __name__ == "__main__":
@@ -223,7 +259,7 @@ if __name__ == "__main__":
 
     ct.add_waiting_gates()
 
-    ct.get_order()
+    ct.order()
 
     ct.plot()
 
---
+++ test_circuit.py
@@ -0,0 +1,187 @@
+import circuit
+
+from unittest.mock import MagicMock, patch, call
+
+import numpy as np
+
+
+class TestCircuit:
+    def test_add_qubit(self):
+        c = circuit.Circuit()
+
+        c.add_qubit(circuit.Qubit("A", t1=10, t2=20))
+
+        assert len(c.gates) == 0
+        assert len(c.qubits) == 1
+        assert c.qubits[0].name == "A"
+
+    def test_order_no_meas(self):
+        c = circuit.Circuit()
+
+        qb = circuit.Qubit("A", t1=10, t2=0)
+        c.add_qubit(qb)
+
+        c.add_gate(circuit.Hadamard("A", time=1))
+        c.add_gate(circuit.Hadamard("A", time=0))
+
+        c.order()
+
+        assert len(c.gates) == 2
+        assert c.gates[0].time == 0
+
+    def test_add_waiting(self):
+        c = circuit.Circuit()
+
+        qb = circuit.Qubit("A", t1=10, t2=0)
+        c.add_qubit(qb)
+
+        c.add_gate(circuit.Hadamard("A", time=1))
+        c.add_gate(circuit.Hadamard("A", time=0))
+
+        assert len(c.gates) == 2
+
+        c.add_waiting_gates()
+
+        assert len(c.gates) == 3
+
+        c.order()
+
+        assert c.gates[1].time == 0.5
+
+    def test_apply_to(self):
+        sdm = MagicMock()
+        sdm.hadamard = MagicMock()
+        sdm.amp_ph_damping = MagicMock()
+        sdm.peak_measurement = MagicMock(return_value = (1,0))
+        sdm.project_measurement = MagicMock()
+
+        c = circuit.Circuit()
+
+        c.add_qubit(circuit.Qubit("A", 10, np.inf))
+        c.add_gate(circuit.Hadamard("A", 0))
+        c.add_gate(circuit.Hadamard("A", 10))
+
+        c.add_gate(circuit.Measurement("A", 20, sampler=None))
+
+        c.add_waiting_gates()
+
+        c.order()
+
+        c.apply_to(sdm)
+
+        gamma = 1-np.exp(-1)
+
+        sdm.assert_has_calls([call.hadamard("A"), 
+                call.amp_ph_damping("A", gamma=gamma, lamda=0),
+                call.hadamard("A"),
+                call.amp_ph_damping("A", gamma=gamma, lamda=0),
+                call.peak_measurement("A"),
+                call.project_measurement("A", 0)])
+
+class TestHadamardGate:
+    def test_init(self):
+        h = circuit.Hadamard("A", 7)
+        assert h.time == 7
+        assert h.involves_qubit("A")
+        assert not h.involves_qubit("B")
+        assert not h.is_measurement
+
+    def test_apply(self):
+        sdm = MagicMock()
+        sdm.hadamard = MagicMock()
+        h = circuit.Hadamard("A", 7)
+        h.apply_to(sdm)
+        sdm.hadamard.assert_called_once_with("A")
+
+class TestCPhaseGate:
+    def test_init(self):
+        cp = circuit.CPhase("A", "B", 10)
+        assert cp.time == 10
+        assert set(cp.involved_qubits) == {"A", "B"}
+        assert not cp.is_measurement
+
+    def test_apply(self):
+        sdm = MagicMock()
+        sdm.cphase = MagicMock()
+
+        cp = circuit.CPhase("A", "B", 10)
+        
+        cp.apply_to(sdm)
+
+        sdm.cphase.assert_called_once_with("A", "B")
+
+class TestAmpPhDamping:
+    def test_init(self):
+        apd = circuit.AmpPhDamp("A", 0, 1, 10, 10)
+        assert apd.time == 0
+        assert apd.duration == 1
+        assert apd.involves_qubit("A")
+        assert not apd.is_measurement
+        
+
+    def test_apply(self):
+        sdm = MagicMock()
+        sdm.amp_ph_damping = MagicMock()
+
+        apd = circuit.AmpPhDamp("A", 0, 1, 10, 5)
+        
+        apd.apply_to(sdm)
+
+        g = 1 - np.exp(-1/10)
+        l = 1 - np.exp(-1/5)
+        sdm.amp_ph_damping.assert_called_once_with("A", gamma=g, lamda=l)
+
+class TestMeasurement:
+    def test_init(self):
+        m = circuit.Measurement("A", 0, sampler=None)
+        assert m.is_measurement
+        assert m.involves_qubit("A")
+        assert m.time == 0
+        
+    def test_apply(self):
+        m = circuit.Measurement("A", 0, sampler=None)
+
+        sdm = MagicMock()
+        sdm.peak_measurement = MagicMock(return_value=(0, 1))
+        sdm.project_measurement = MagicMock()
+
+        m.apply_to(sdm)
+
+        assert m.measurements == [1]
+
+        sdm.peak_measurement.assert_called_once_with("A")
+        sdm.project_measurement.assert_called_once_with("A", 1)
+
+        sdm.peak_measurement = MagicMock(return_value=(1, 0))
+        sdm.project_measurement = MagicMock()
+
+        m.apply_to(sdm)
+
+        assert m.measurements == [1, 0]
+
+        sdm.peak_measurement.assert_called_once_with("A")
+        sdm.project_measurement.assert_called_once_with("A", 0)
+
+
+    def test_apply_random(self):
+        m = circuit.Measurement("A", 0, sampler=None)
+
+        with patch('numpy.random.random') as p:
+            p.return_value = 0.3
+            sdm = MagicMock()
+            sdm.peak_measurement = MagicMock(return_value=(0.06, 0.04))
+            sdm.project_measurement = MagicMock()
+
+            m.apply_to(sdm)
+
+            sdm.project_measurement.assert_called_once_with("A", 0)
+
+            sdm.peak_measurement = MagicMock(return_value=(0.06, 0.04))
+            sdm.project_measurement = MagicMock()
+            p.return_value = 0.7
+
+            m.apply_to(sdm)
+
+            sdm.project_measurement.assert_called_once_with("A", 1)
+
+
