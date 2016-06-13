e0134366f7e622ccb8515c0472429c6df9412f62
Brian Tarasinski <brianzi@physik.fu-berlin.de>
2016-06-13 14:48:58 +0200

    make module: change setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+import setuptools
+from distutils.core import setup, Extension
 from Cython.Build import cythonize
 
 setup(name='quantumsim',
-    version='0.1',
-    description='Simulation of quantum circuits under somewhat realistic condititons',
-    author='Brian Tarasinski et al',
-    author_email='brianzi@physik.fu-berlin.de',
-    ext_modules = cythonize("dmcpu.pyx"),
-    py_modules=['dmcpu', 'dm10', 'sparsedm', 'circuit']
-
-    data_files=[('kernelsource', ['primitives.cu'])]
-    )
+      version='0.1',
+      description='Simulation of quantum circuits under somewhat realistic condititons',
+      author='Brian Tarasinski et al',
+      author_email='brianzi@physik.fu-berlin.de',
+      ext_modules=cythonize(Extension("dmcpu", ["quantumsim/dmcpu.pyx"])),
+      packages=['quantumsim'],
+      ext_package='quantumsim',
+      data_files=[('pycudakernels', ['src/primitives.cu'])]
+      )
