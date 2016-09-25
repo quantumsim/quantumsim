4c55d7161c8997ab671f372fc7f74a3f7ddb1fac
Brian Tarasinski <brianzi@physik.fu-berlin.de>
2016-09-25 11:04:21 +0200

    add two_qubit_ptm and first test

--- quantumsim/primitives.cu
+++ quantumsim/primitives.cu
@@ -110,7 +110,6 @@ __global__ void cphase(double *dm, unsigned int bit0, unsigned int bit1, unsigne
 // apply a 4x4 pauli transfer matrix (in 0, x, y, 1 basis)
 // to the specified qubit
 __global__ void single_qubit_ptm(double *dm, double *ptm_g,  unsigned int bit, unsigned int no_qubits) {
-
     const unsigned int x = threadIdx.x;
     const unsigned int high_x = blockIdx.x * blockDim.x;
 
@@ -153,6 +152,64 @@ __global__ void single_qubit_ptm(double *dm, double *ptm_g,  unsigned int bit, u
     acc += ptm[4*row + 3] * data[idx + 3];
 
     //upload back to global memory
+    __syncthreads();
+    dm[global_from] = acc;
+}
+
+
+__global__ void two_qubit_ptm(double *dm, double *ptm_g, unsigned int bit0, unsigned int bit1, unsigned int no_qubits) {
+    const unsigned int x = threadIdx.x;
+    const unsigned int high_x = blockIdx.x * blockDim.x;
+
+    const int global_from = 
+
+    if (high_x + x >= (1 << (2*no_qubits))) return;
+
+    extern __shared__ double ptm[];
+    double *data = &ptm[256]; //need blockDim.x double floats
+
+
+
+    // the lowest to bits of x are used to address bit0, the next two are used to address bit1 
+    // global address = <- pos = 
+    // aaaxxbbbbyycccc  <- aaabbbbccccxxyy
+
+    int higher_bit = max(bit0, bit1);
+    int lower_bit = min(bit0, bit1);
+    int high_mask = ~ ( (1 << (2*higher_bit+2)) - 1 ); //a mask (of pos)
+    int mid_mask = (~ ( (1 << (2*lower_bit + 4)) - 1)) & (~high_mask);  //b mask
+    int low_mask  = ~(high_mask | mid_mask) & (~0xf);  //c mask
+
+    int pos = high_x | x;
+    int global_from = 
+              (pos & high_mask) 
+            | ((pos & mid_mask) >> 2)
+            | ((pos & low_mask) >> 4)
+            | ((pos & 0x3) << (2 * bit0))  
+            | ((pos & 0xc)  << (2 * bit1));
+
+    //fetch ptm to shared memmory
+    //need to fetch several values per thread if blockDim.x is less than 256 (only for small dms...)
+    for(int i=0; i < 256; i+=blockDim.x) {
+        if(i+x < 256) {
+            ptm[i+x] = ptm_g[i+x];
+        }
+    }
+
+    //fetch data block to shared memory
+    data[x] = dm[global_from];
+    __syncthreads();
+
+    int row = x & 0xf;
+    int idx = x & ~0xf;
+
+    double acc=0;
+    for(int i=0; i<16; i++) {
+
+        acc += ptm[16*row + i]*data[16*idx+i];
+
+    }
+
     __syncthreads();
     dm[global_from] = acc;
 }
--- quantumsim/test/test_primitives.py
+++ quantumsim/test/test_primitives.py
@@ -1,10 +1,10 @@
 import pytest
 import numpy as np
 
-import matplotlib.pyplot as plt
 hascuda = True
 try:
     import pycuda.autoinit
+    pycuda.autoinit
     import pycuda.driver as drv
     from pycuda.compiler import SourceModule
 
@@ -19,15 +19,16 @@ try:
     bit_to_pauli_basis = mod.get_function("bit_to_pauli_basis")
     pauli_reshuffle = mod.get_function("pauli_reshuffle")
     single_qubit_ptm = mod.get_function("single_qubit_ptm")
-    trace  = mod.get_function("trace")
+    trace = mod.get_function("trace")
     swap = mod.get_function("swap")
+    two_qubit_ptm = mod.get_function("two_qubit_ptm")
 except ImportError:
     hascuda = False
 
 
 no_qubits = 10
-block = (32,32,1)
-grid = (1<<(no_qubits-5), 1<<(no_qubits-5), 1)
+block = (32, 32, 1)
+grid = (1 << (no_qubits - 5), 1 << (no_qubits - 5), 1)
 
 x = np.random.random((2**no_qubits, 2**no_qubits)).astype(np.complex128)
 x += np.random.random((2**no_qubits, 2**no_qubits)) * 1j
@@ -36,6 +37,7 @@ x = np.dot(x.T.conj(), x)
 x = x / np.trace(x)
 
 pytestmark = pytest.mark.skipif(not hascuda, reason="pycuda not installed")
+
 
 def random_dm10():
     "return a random (2**no_qubits, 2**no_qubits) density matrix"
@@ -50,8 +52,8 @@ class TestToPauli:
         dm_gpu = drv.to_device(dm)
 
         for i in range(no_qubits):
-            bit_to_pauli_basis(dm_gpu, np.int32(1<<i), np.int32(no_qubits),
-                    block=block, grid=grid)
+            bit_to_pauli_basis(dm_gpu, np.int32(1 << i), np.int32(no_qubits),
+                               block=block, grid=grid)
 
         dm2 = drv.from_device_like(dm_gpu, dm)
 
@@ -63,13 +65,13 @@ class TestToPauli:
         dm_gpu = drv.to_device(dm)
 
         for i in range(no_qubits):
-            bit_to_pauli_basis(dm_gpu, np.int32(1<<i), np.int32(no_qubits),
-                    block=block, grid=grid)
+            bit_to_pauli_basis(dm_gpu, np.int32(1 << i), np.int32(no_qubits),
+                               block=block, grid=grid)
 
         dm2 = drv.from_device_like(dm_gpu, dm)
 
         where_real = dm2.real == dm2
-        where_imag = 1j*dm2.imag == dm2
+        where_imag = 1j * dm2.imag == dm2
         where_all = where_real + where_imag
 
         assert np.all(where_all)
@@ -80,12 +82,12 @@ class TestToPauli:
         dm_gpu = drv.to_device(dm)
 
         for i in range(no_qubits):
-            bit_to_pauli_basis(dm_gpu, np.int32(1<<i), np.int32(no_qubits),
-                    block=block, grid=grid)
+            bit_to_pauli_basis(dm_gpu, np.int32(1 << i), np.int32(no_qubits),
+                               block=block, grid=grid)
 
         for i in range(no_qubits):
-            bit_to_pauli_basis(dm_gpu, np.int32(1<<i), np.int32(no_qubits),
-                    block=block, grid=grid)
+            bit_to_pauli_basis(dm_gpu, np.int32(1 << i), np.int32(no_qubits),
+                               block=block, grid=grid)
 
         dm2 = drv.from_device_like(dm_gpu, dm)
 
@@ -97,124 +99,136 @@ class TestToPauli:
         dm_gpu = drv.to_device(dm)
 
         for i in range(no_qubits):
-            bit_to_pauli_basis(dm_gpu, np.int32(1<<i), np.int32(no_qubits),
-                    block=block, grid=grid)
+            bit_to_pauli_basis(dm_gpu, np.int32(1 << i), np.int32(no_qubits),
+                               block=block, grid=grid)
 
-        dmreal = np.zeros(2**(2*no_qubits))
+        dmreal = np.zeros(2**(2 * no_qubits))
         dmreal_gpu = drv.to_device(dmreal)
 
         pauli_reshuffle(dm_gpu, dmreal_gpu, np.int32(no_qubits), np.int32(0),
-                block=block, grid=grid)
+                        block=block, grid=grid)
 
         dm_gpu2 = drv.mem_alloc(dm.nbytes)
         drv.memset_d8(dm_gpu2, 0, dm.nbytes)
 
         pauli_reshuffle(dm_gpu2, dmreal_gpu, np.int32(no_qubits), np.int32(1),
-                block=block, grid=grid)
+                        block=block, grid=grid)
 
         for i in range(no_qubits):
-            bit_to_pauli_basis(dm_gpu2, np.int32(1<<i), np.int32(no_qubits),
-                    block=block, grid=grid)
+            bit_to_pauli_basis(dm_gpu2, np.int32(1 << i), np.int32(no_qubits),
+                               block=block, grid=grid)
 
         dm2 = drv.from_device_like(dm_gpu2, dm)
 
         assert np.allclose(dm, dm2)
 
+
 class TestTrace:
 
     def test_size_one(self):
         n = 1
-        x = np.random.random(n);
+        x = np.random.random(n)
 
         x_gpu = drv.to_device(x)
-        
-        trace(x_gpu, np.int32(-1), block=(n,1,1), grid=(1,1,1), shared = 8*128)
+
+        trace(x_gpu, np.int32(-1), block=(n, 1, 1),
+              grid=(1, 1, 1), shared=8 * 128)
 
         x2 = drv.from_device_like(x_gpu, x)
-        
 
         assert np.allclose(x2[0], np.sum(x))
 
     def test_simple(self):
         n = 32
-        x = np.random.random(n);
+        x = np.random.random(n)
 
         x_gpu = drv.to_device(x)
-        
-        trace(x_gpu, np.int32(-1), block=(n,1,1), grid=(1,1,1), shared = 8*128)
+
+        trace(x_gpu, np.int32(-1), block=(n, 1, 1),
+              grid=(1, 1, 1), shared=8 * 128)
 
         x2 = drv.from_device_like(x_gpu, x)
-        
 
         assert np.allclose(x2[0], np.sum(x))
 
     def test_large(self):
         n = 1024
-        x = np.random.random(n);
+        x = np.random.random(n)
 
         x_gpu = drv.to_device(x)
-        
-        trace(x_gpu, np.int32(-1), block=(n,1,1), grid=(1,1,1), shared = 8*n)
+
+        trace(x_gpu, np.int32(-1), block=(n, 1, 1),
+              grid=(1, 1, 1), shared=8 * n)
 
         x2 = drv.from_device_like(x_gpu, x)
-        
+
         assert np.allclose(x2[0], np.sum(x))
 
     def test_sum_bit0(self):
 
-        n = 32 
-        x = np.random.random(n);
+        n = 32
+        x = np.random.random(n)
         # x = np.arange(n).astype(np.float64)
 
         x_gpu = drv.to_device(x)
-        
-        trace(x_gpu, np.int32(0), block=(n,1,1), grid=(1,1,1), shared = 8*128)
+
+        trace(
+            x_gpu, np.int32(0), block=(
+                n, 1, 1), grid=(
+                1, 1, 1), shared=8 * 128)
 
         x2 = drv.from_device_like(x_gpu, x)
 
         print(x)
         print(x2)
-        
+
         assert np.allclose(x2[1], np.sum(x[::2]))
         assert np.allclose(x2[0], np.sum(x[1::2]))
 
     def test_sum_bit1(self):
 
-        n = 32 
-        x = np.random.random(n);
+        n = 32
+        x = np.random.random(n)
         # x = np.arange(n).astype(np.float64)
 
         x_gpu = drv.to_device(x)
-        
-        trace(x_gpu, np.int32(1), block=(n,1,1), grid=(1,1,1), shared = 8*128)
+
+        trace(
+            x_gpu, np.int32(1), block=(
+                n, 1, 1), grid=(
+                1, 1, 1), shared=8 * 128)
 
         x2 = drv.from_device_like(x_gpu, x)
 
         print(x)
         print(x2)
-        
+
         assert np.allclose(x2[1], np.sum(x[::4]) + np.sum(x[1::4]))
         assert np.allclose(x2[0], np.sum(x[2::4]) + np.sum(x[3::4]))
 
     def test_sum_bit_high(self):
 
-        n = 32 
-        x = np.random.random(n);
+        n = 32
+        x = np.random.random(n)
         # x = np.arange(n).astype(np.float64)
 
         x_gpu = drv.to_device(x)
-        
-        trace(x_gpu, np.int32(4), block=(n,1,1), grid=(1,1,1), shared = 8*128)
+
+        trace(
+            x_gpu, np.int32(4), block=(
+                n, 1, 1), grid=(
+                1, 1, 1), shared=8 * 128)
 
         x2 = drv.from_device_like(x_gpu, x)
 
         print(x)
         print(x2)
-        
+
         assert np.allclose(x2[1], np.sum(x[:16]))
         assert np.allclose(x2[0], np.sum(x[16:]))
 
-class TestPTM:
+
+class TestOneBitPTM:
 
     def test_identity(self):
         ptm = np.eye(4)
@@ -223,31 +237,33 @@ class TestPTM:
 
         dm = np.random.random((512, 512))
 
-
         dm_gpu = drv.to_device(dm)
 
-        single_qubit_ptm(dm_gpu, ptm_gpu, np.int32(2), np.int32(9), block=(512,1,1), grid=(512,1,1), shared = 8*(16 + 512))
+        single_qubit_ptm(dm_gpu, ptm_gpu, np.int32(2), np.int32(
+            9), block=(512, 1, 1), grid=(512, 1, 1), shared=8 * (16 + 512))
 
         dm2 = drv.from_device_like(dm_gpu, dm)
 
         assert np.allclose(dm2, dm)
 
     def test_xy_hadamard_squares_to_one(self):
-        ptm = np.array( [[1, 0, 0, 0], [0, 0, 1, 0], [0, 1, 0, 0], [0, 0, 0, 1]], np.float64)
+        ptm = np.array([[1, 0, 0, 0], [0, 0, 1, 0], [
+                       0, 1, 0, 0], [0, 0, 0, 1]], np.float64)
 
         ptm_gpu = drv.to_device(ptm)
 
         dm = np.random.random((512, 512))
 
-
         dm_gpu = drv.to_device(dm)
 
-        single_qubit_ptm(dm_gpu, ptm_gpu, np.int32(2), np.int32(9), block=(512,1,1), grid=(512,1,1), shared = 8*(16 + 512))
+        single_qubit_ptm(dm_gpu, ptm_gpu, np.int32(2), np.int32(
+            9), block=(512, 1, 1), grid=(512, 1, 1), shared=8 * (16 + 512))
 
         dm2 = drv.from_device_like(dm_gpu, dm)
         assert not np.allclose(dm, dm2)
 
-        single_qubit_ptm(dm_gpu, ptm_gpu, np.int32(2), np.int32(9), block=(512,1,1), grid=(512,1,1), shared = 8*(16 + 512))
+        single_qubit_ptm(dm_gpu, ptm_gpu, np.int32(2), np.int32(
+            9), block=(512, 1, 1), grid=(512, 1, 1), shared=8 * (16 + 512))
 
         dm2 = drv.from_device_like(dm_gpu, dm)
 
@@ -255,26 +271,46 @@ class TestPTM:
 
     def test_normal_hadamard_squares_to_one(self):
 
-        ptm = np.array( 
-                [[0.5, np.sqrt(0.5), 0, 0.5], 
-                 [np.sqrt(0.5), 0, 0, -np.sqrt(0.5)], 
-                 [0, 0, -1, 0], 
-                 [0.5, -np.sqrt(0.5), 0, 0.5]], 
-                np.float64)
+        ptm = np.array(
+            [[0.5, np.sqrt(0.5), 0, 0.5],
+             [np.sqrt(0.5), 0, 0, -np.sqrt(0.5)],
+             [0, 0, -1, 0],
+             [0.5, -np.sqrt(0.5), 0, 0.5]],
+            np.float64)
 
         ptm_gpu = drv.to_device(ptm)
 
         dm = np.random.random((512, 512))
 
-
         dm_gpu = drv.to_device(dm)
 
-        single_qubit_ptm(dm_gpu, ptm_gpu, np.int32(2), np.int32(9), block=(512,1,1), grid=(512,1,1), shared = 8*(16 + 512))
+        single_qubit_ptm(dm_gpu, ptm_gpu, np.int32(2), np.int32(
+            9), block=(512, 1, 1), grid=(512, 1, 1), shared=8 * (16 + 512))
 
         dm2 = drv.from_device_like(dm_gpu, dm)
         assert not np.allclose(dm, dm2)
 
-        single_qubit_ptm(dm_gpu, ptm_gpu, np.int32(2), np.int32(9), block=(512,1,1), grid=(512,1,1), shared = 8*(16 + 512))
+        single_qubit_ptm(dm_gpu, ptm_gpu, np.int32(2), np.int32(
+            9), block=(512, 1, 1), grid=(512, 1, 1), shared=8 * (16 + 512))
+
+        dm2 = drv.from_device_like(dm_gpu, dm)
+
+        assert np.allclose(dm2, dm)
+
+
+class TestTwoBitPTM:
+
+    def test_identity_big(self):
+        ptm = np.eye(16)
+
+        ptm_gpu = drv.to_device(ptm)
+
+        dm = np.random.random((512, 512))
+
+        dm_gpu = drv.to_device(dm)
+
+        single_qubit_ptm(dm_gpu, ptm_gpu, np.int32(2), np.int32(6), np.int32(
+            9), block=(512, 1, 1), grid=(512, 1, 1), shared=8 * (16 + 512))
 
         dm2 = drv.from_device_like(dm_gpu, dm)
 
@@ -282,23 +318,28 @@ class TestPTM:
 
 
 class TestSwap:
+
     def test_stupid(self):
 
         n = 8
 
-        x = np.random.random(2**(2*n))
+        x = np.random.random(2**(2 * n))
 
         x_gpu = drv.to_device(x)
 
         block = (128, 1, 1)
-        grid = (2**(2*n) // 128, 1, 1)
+        grid = (2**(2 * n) // 128, 1, 1)
 
-
-        swap(x_gpu, np.int32(4), np.int32(5), np.int32(n), grid=grid, block=block) 
+        swap(
+            x_gpu,
+            np.int32(4),
+            np.int32(5),
+            np.int32(n),
+            grid=grid,
+            block=block)
 
         x2 = drv.from_device_like(x_gpu, x)
 
         assert np.allclose(np.sum(x2), np.sum(x))
 
         assert not np.allclose(x, x2)
-
