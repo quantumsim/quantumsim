3f5f7157c3db0cc81b22d3fcd9fda9f2353f0ea6
Brian Tarasinski <brianzi@physik.fu-berlin.de>
2017-12-05 02:02:14 +0100

    Initial design proper framework for superoperators.
    So far only for single-qubit PTMs, including ported tests.

--- quantumsim/ptm.py
+++ quantumsim/ptm.py
@@ -1,7 +1,10 @@
 import numpy as np
+import collections
+
+import scipy.linalg
 
 
-"The transformation matrix between the two basis. Its essentially a Hadamard, so its its own inverse."
+"The transformation matrix between the two bases. Its essentially a Hadamard, so its its own inverse."
 basis_transformation_matrix = np.array([[np.sqrt(0.5), 0, 0, np.sqrt(0.5)],
                                         [0, 1, 0, 0],
                                         [0, 0, 1, 0],
@@ -19,7 +22,7 @@ _ptm_basis_vectors_cache = {}
 def general_ptm_basis_vector(n):
     """
     The vector of 'Pauli matrices' in dimension n.
-    First the n diagonal matrices, then 
+    First the n diagonal matrices, then
     the off-diagonals in x-like, y-like pairs
     """
 
@@ -36,16 +39,16 @@ def general_ptm_basis_vector(n):
 
         for i in range(n):
             for j in range(i):
-                #x-like
+                # x-like
                 v = np.zeros((n, n), np.complex)
-                v[i, j] = np.sqrt(0.5) 
-                v[j, i] = np.sqrt(0.5) 
+                v[i, j] = np.sqrt(0.5)
+                v[j, i] = np.sqrt(0.5)
                 basis_vector.append(v)
 
-                #y-like
+                # y-like
                 v = np.zeros((n, n), np.complex)
-                v[i, j] = 1j*np.sqrt(0.5) 
-                v[j, i] = -1j*np.sqrt(0.5) 
+                v[i, j] = 1j * np.sqrt(0.5)
+                v[j, i] = -1j * np.sqrt(0.5)
                 basis_vector.append(v)
 
         basis_vector = np.array(basis_vector)
@@ -59,7 +62,7 @@ def to_0xy1_basis(ptm, general_basis=False):
     """Transform a Pauli transfer in the "usual" basis (0xyz) [1],
     to the 0xy1 basis which is required by sparsesdm.apply_ptm.
 
-    If general_basis is True, transform to the 01xy basis, which is the 
+    If general_basis is True, transform to the 01xy basis, which is the
     two-qubit version of the general basis defined by ptm.general_ptm_basis_vector().
 
     ptm: The input transfer matrix in 0xyz basis. Can be 4x4, 4x3 or 3x3 matrix of real numbers.
@@ -91,7 +94,6 @@ def to_0xy1_basis(ptm, general_basis=False):
     return result
 
 
-
 def to_0xyz_basis(ptm):
     """Transform a Pauli transfer in the 0xy1 basis [1],
     to the the usual 0xyz. The inverse of to_0xy1_basis.
@@ -106,16 +108,20 @@ def to_0xyz_basis(ptm):
         trans_mat = basis_transformation_matrix
         return np.dot(trans_mat, np.dot(ptm, trans_mat))
     elif ptm.shape == (16, 16):
-        trans_mat = np.kron(basis_transformation_matrix, basis_transformation_matrix)
+        trans_mat = np.kron(
+            basis_transformation_matrix,
+            basis_transformation_matrix)
         return np.dot(trans_mat, np.dot(ptm, trans_mat))
     else:
-        raise ValueError("Dimensions wrong, must be one- or two Pauli transfer matrix ")
+        raise ValueError(
+            "Dimensions wrong, must be one- or two Pauli transfer matrix ")
+
 
 def hadamard_ptm(general_basis=False):
     """Return a 4x4 Pauli transfer matrix in 0xy1 basis,
     representing perfect unitary Hadamard (Rotation around the (x+z)/sqrt(2) axis by π).
     """
-    u = np.array([[1, 1], [1, -1]])*np.sqrt(0.5)
+    u = np.array([[1, 1], [1, -1]]) * np.sqrt(0.5)
     return single_kraus_to_ptm(u, general_basis)
 
 
@@ -132,22 +138,23 @@ def amp_ph_damping_ptm(gamma, lamda, general_basis=False):
     )
     return to_0xy1_basis(ptm, general_basis)
 
+
 def gen_amp_damping_ptm(gamma_down, gamma_up):
     """Return a 4x4 Pauli transfer matrix  representing amplitude damping including an excitation rate gamma_up.
     """
 
     gamma = gamma_up + gamma_down
-    p = gamma_down/(gamma_down+gamma_up)
-
+    p = gamma_down / (gamma_down + gamma_up)
 
     ptm = np.array([
         [1, 0, 0, 0],
         [0, np.sqrt((1 - gamma)), 0, 0],
         [0, 0, np.sqrt((1 - gamma)), 0],
-        [(2*p - 1)*gamma, 0, 0, 1 - gamma]]
+        [(2 * p - 1) * gamma, 0, 0, 1 - gamma]]
     )
-    
+
     return to_0xy1_basis(ptm)
+
 
 def dephasing_ptm(px, py, pz):
     """Return a 4x4 Pauli transfer matrix in 0xy1 basis,
@@ -203,7 +210,9 @@ def single_kraus_to_ptm_general(kraus):
 
     st = general_ptm_basis_vector(d)
 
-    return np.einsum("xab, bc, ycd, ad -> xy", st, kraus, st, kraus.conj()).real
+    return np.einsum("xab, bc, ycd, ad -> xy", st,
+                     kraus, st, kraus.conj()).real
+
 
 def single_kraus_to_ptm(kraus, general_basis=False):
     """Given a Kraus operator in z-basis, obtain the corresponding single-qubit ptm in 0xy1 basis"""
@@ -211,7 +220,9 @@ def single_kraus_to_ptm(kraus, general_basis=False):
         st = general_ptm_basis_vector(2)
     else:
         st = single_tensor
-    return np.einsum("xab, bc, ycd, ad -> xy", st, kraus, st, kraus.conj()).real
+    return np.einsum("xab, bc, ycd, ad -> xy", st,
+                     kraus, st, kraus.conj()).real
+
 
 def double_kraus_to_ptm(kraus, general_basis=False):
     if general_basis:
@@ -221,4 +232,330 @@ def double_kraus_to_ptm(kraus, general_basis=False):
 
     dt = np.kron(st, st)
 
-    return np.einsum("xab, bc, ycd, ad -> xy", dt, kraus, dt, kraus.conj()).real
+    return np.einsum("xab, bc, ycd, ad -> xy", dt,
+                     kraus, dt, kraus.conj()).real
+
+
+class PauliBasis:
+    def __init__(self, basisvectors=None):
+        """
+        Defines a Pauli basis [1]. The number of element vectors is given by `dim_pauli`,
+        while the dimension of the hilbert space is given by dim_hilbert.
+
+        For instance, for a qubit (Hilbert space dimension d=2), one could employ a Pauli basis
+        with dimension d**2 = 4, or, if one wants do describe a classical state (mixture of |0> and |1>),
+        use a smaller basis with only d_pauli = 2.
+
+        [1] A Pauli basis is an orthonormal basis (w.r.t <A, B> = Tr(A.B+)) for a space of Hermitian matrices.
+        """
+
+        "a tensor B of shape (dim_pauli, dim_hilbert, dim_hilbert)"
+        "read as a vector of matrices"
+        "must satisfy Tr(B[i] @ B[j]) = delta(i, j)"
+        if basisvectors is not None:
+            self.basisvectors = basisvectors
+
+        shape = self.basisvectors.shape
+
+        assert shape[1] == shape[2]
+
+        self.dim_hilbert = shape[1]
+        self.dim_pauli = shape[0]
+
+        self.computational_basis_vectors = np.einsum("xii -> ix", self.basisvectors)
+
+    def hilbert_to_pauli_vector(self, rho):
+        return np.einsum("xab, ba -> x", self.basisvectors, rho)
+
+
+class GeneralBasis(PauliBasis):
+    def __init__(self, dim):
+        self.basisvectors = general_ptm_basis_vector(dim)
+        super().__init__()
+
+
+class PauliBasis_0xy1(PauliBasis):
+    "the pauli basis used by older versions of quantumsim"
+    basisvectors = np.array([[[1, 0], [0, 0]],
+                             np.sqrt(0.5) * np.array([[0, 1], [1, 0]]),
+                             np.sqrt(0.5) * np.array([[0, -1j], [1j, 0]]),
+                             [[0, 0], [0, 1]]])
+
+
+class PauliBasis_exyz(PauliBasis):
+    "standard Pauli basis for a qubit"
+    basisvectors = np.sqrt(0.5) * np.array([[[1, 0], [0, 1]],
+                                            [[0, 1], [1, 0]],
+                                            [[0, -1j], [1j, 0]],
+                                            [[1, 0], [0, -1]]])
+
+
+class PTM:
+    def __init__(self):
+        """
+        A Pauli transfer matrix. ABC
+        """
+
+        "the hilbert space dimension on which the PTM operates"
+        self.dim_hilbert = ()
+        raise NotImplementedError
+
+    def get_matrix(self, in_basis, out_basis=None):
+        """
+        Return the matrix representation of this PTM in the basis given.
+
+        If out_basis is None, in_basis = out_basis is assumed.
+
+        If out_basis spans only a subspace, projection on that subspace is implicit.
+        """
+        raise NotImplementedError
+
+    def __add__(self, other):
+        assert isinstance(other, PTM), "cannot add PTM to non-ptm"
+        if isinstance(other, LinearCombPTM):
+            return LinearCombPTM(self.elements + other.elements)
+        else:
+            return LinearCombPTM({self: 1, other: 1})
+
+    def __mul__(self, scalar):
+        return LinearCombPTM({self: scalar})
+
+    # vector space boiler plate...
+
+    def __radd__(self, other):
+        return self.__add__(other)
+
+    def __rmul__(self, other):
+        return self.__mul__(other)
+
+    def __neg__(self):
+        return self.__mul__(-1)
+
+    def __sub__(self, other):
+        if isinstance(other, LinearCombPTM):
+            return LinearCombPTM(self.elements - other.elements)
+        else:
+            return LinearCombPTM(self.elements - collections.Counter([other]))
+
+    def __rsub__(self, other):
+        return other.__sub__(self)
+
+    def __matmul__(self, other):
+        return ProductPTM([self, other])
+
+
+class LinearCombPTM(PTM):
+    def __init__(self, elements):
+        """
+        A linear combination of other PTMs.
+        Should usually not be instantiated by hand, but created as a result of adding or scaling PTMs.
+        """
+
+        # check dimensions
+        dimensions_set = set(p.dim_hilbert for p in elements.keys())
+        if len(dimensions_set) > 1:
+            raise ValueError(
+                "cannot create linear combination: incompatible dimensions: {}".format(dimensions_set))
+
+        self.dim_hilbert = dimensions_set.pop()
+
+        # float defaultdict of shape {ptm: coefficient, ...}
+        self.elements = collections.Counter(elements)
+
+    def get_matrix(self, in_basis, out_basis=None):
+        return sum(c * p.get_matrix(in_basis, out_basis)
+                   for p, c in self.elements.items())
+
+    def __mul__(self, scalar):
+        return LinearCombPTM({p: scalar * c for p, c in self.elements.items()})
+
+    def __add__(self, other):
+        assert isinstance(other, PTM), "cannot add PTM to non-ptm"
+        if isinstance(other, LinearCombPTM):
+            return LinearCombPTM(self.elements + other.elements)
+        else:
+            return LinearCombPTM(self.elements + collections.Counter([other]))
+
+class ProductPTM(PTM):
+    def __init__(self, elements):
+        """
+        A product of other PTMs.
+        Should usually not be instantiated by hand, but created as a result of multiplying PTMs.
+
+        elements: list of factors. Will be multiplied in order elements[n-1] @ ... @ elements[0].
+        """
+
+        # check dimensions
+        dimensions_set = set(p.dim_hilbert for p in elements)
+        if len(dimensions_set) > 1:
+            raise ValueError(
+                "cannot create product: incompatible dimensions: {}".format(dimensions_set))
+
+        elif len(dimensions_set) == 1:
+            self.dim_hilbert = dimensions_set.pop()
+        else:
+            self.dim_hilbert = None
+
+        self.elements = elements
+
+    def get_matrix(self, basis_in, basis_out=None):
+        # FIXME: product is always formed in the complete basis, which might be
+        # not efficient
+        if basis_out is None:
+            basis_out = basis_in
+
+        assert basis_in.dim_hilbert == basis_out.dim_hilbert
+
+        if self.dim_hilbert:
+            assert basis_in.dim_hilbert == self.dim_hilbert
+
+        complete_basis = GeneralBasis(basis_in.dim_hilbert)
+        result = np.eye(complete_basis.dim_pauli)
+        for pi in self.elements:
+            pi_mat = pi.get_matrix(complete_basis)
+            result = result @ pi_mat
+
+        trans_mat_in = np.einsum("xab, yba", complete_basis.basisvectors, basis_in.basisvectors.conj())
+        trans_mat_out = np.einsum("xab, yba", basis_out.basisvectors, complete_basis.basisvectors.conj())
+
+        return trans_mat_out @ result @ trans_mat_in
+
+    def __matmul__(self, other):
+        if isinstance(other, ProductPTM):
+            return ProductPTM(self.elements + other.elements)
+        else:
+            return ProductPTM(self.elements + [other])
+
+    def __rmatmul__(self, other):
+        return other.__matmul__(self)
+
+class ConjunctionPTM(PTM):
+    def __init__(self, op):
+        """
+        The PTM describing conjunction with unitary operator `op`, i.e.
+
+        rho' = P(rho) = op . rho . op^dagger
+
+        `op` is a matrix given in computational basis.
+
+        Typical usage: op describes the unitary time evolution of a system described by rho.
+        """
+        self.op = np.array(op)
+        self.dim_hilbert = self.op.shape[0]
+
+    def get_matrix(self, basis_in, basis_out=None):
+        if basis_out is None:
+            basis_out = basis_in
+
+        assert self.op.shape == (basis_out.dim_hilbert, basis_in.dim_hilbert)
+
+        st_out = basis_out.basisvectors
+        st_in = basis_in.basisvectors
+
+        result = np.einsum("xab, bc, ycd, ad -> xy",
+                           st_out, self.op, st_in, self.op.conj())
+
+        assert np.allclose(result.imag, 0)
+
+        return result.real
+
+
+class AdjunctionPLM(PTM):
+    def __init__(self, op):
+        """
+        The PLM (Pauli Liouvillian Matrix) describing adjunction with operator `op`, i.e.
+
+        rho' = P(rho) =  1j*(op.rho - rho.op)
+
+        Typical usage: op is a Hamiltonian, the PTM describes the infinitesimal evolution of rho.
+
+        """
+        self.op = op
+        self.dim_hilbert = op.shape[0]
+
+    def get_matrix(self, basis_in, basis_out=None):
+        if basis_out is None:
+            basis_out = basis_in
+
+        assert self.op.shape == (basis_out.dim_hilbert, basis_in.dim_hilbert)
+
+        st_out = basis_out.basisvectors
+        st_in = basis_in.basisvectors
+
+        result = 1j * np.einsum("xab, bc, ycd -> xy",
+                                st_out, self.op, st_in)
+
+        # taking the real part implements the two parts of the commutator
+        return result.real
+
+
+class IntegratedPLM(PTM):
+    def __init__(self, plm):
+        """
+        The PTM that arises for applying the Pauli Liouvillian `plm`
+        for one unit of time.
+        """
+        self.plm = plm
+        self.dim_hilbert = plm.dim_hilbert
+
+    def get_matrix(self, basis_in, basis_out=None):
+        if basis_out is None:
+            basis_out = basis_in
+
+        assert self.op.shape == (basis_out.dim_hilbert, basis_in.dim_hilbert)
+
+        # we need to get a square representation!
+        plm_matrix = self.plm.get_matrix(basis_in, basis_in)
+
+        ptm_matrix = scipy.linalg.matfuncs.expm(plm_matrix)
+
+        # then basis-transform to out basis
+        return PTM(ptm_matrix).get_matrix()
+
+class LindbladPLM(PTM):
+    def __init__(self, op):
+        """
+        The PLM describing the Lindblad superoperator of `op`, i.e.
+
+        rho' = P(rho) = op.rho.op^dagger - 1/2 {op^dagger.op, rho}
+
+        Typical usage: op is a decay operator.
+        """
+        self.op = op
+        self.dim_hilbert = op.shape[0]
+
+    def get_matrix(self, basis_in, basis_out=None):
+        if basis_out is None:
+            basis_out = basis_in
+
+        assert self.op.shape == (basis_out.dim_hilbert, basis_in.dim_hilbert)
+
+        st_out = basis_out.basisvectors
+        st_in = basis_in.basisvectors
+
+        result = np.einsum("xab, bc, ycd, ad -> xy",
+                           st_out, self.op, st_in, self.op.conj())
+
+        result -= 0.5 * np.einsum("xab, cb, cd, yda -> xy",
+                                  st_out, self.op.conj(), self.op, st_in)
+
+        result -= 0.5 * np.einsum("xab, ybc, dc, da -> xy",
+                                  st_out, st_in, self.op.conj(), self.op)
+
+        return result.real
+
+
+class RotateXPTM(ConjunctionPTM):
+    def __init__(self, angle):
+        s, c = np.sin(angle/2), np.cos(angle/2)
+        super().__init__([[c, 1j*s], [1j*s, c]])
+
+class RotateYPTM(ConjunctionPTM):
+    def __init__(self, angle):
+        s, c = np.sin(angle/2), np.cos(angle/2)
+        super().__init__([[c, s], [-s, c]])
+
+class RotateZPTM(ConjunctionPTM):
+    def __init__(self, angle):
+        z = np.exp(-.5j*angle)
+        super().__init__([[z, 0], [0, z.conj()]])
--- quantumsim/test/test_ptm.py
+++ quantumsim/test/test_ptm.py
@@ -1,6 +1,10 @@
 import quantumsim.ptm as ptm
 import numpy as np
 
+import pytest
+
+pytest
+
 
 # some states in 0xy1 basis
 ground_state = np.array([1, 0, 0, 0])
@@ -8,12 +12,14 @@ excited_state = np.array([0, 0, 0, 1])
 
 rng = np.random.RandomState()
 
+
 def random_state():
     a = rng.rand(4)
 
     a /= a[0] + a[3]
 
-    return a 
+    return a
+
 
 class TestPTMBasisConversion:
     def test_convert_unity(self):
@@ -23,11 +29,11 @@ class TestPTMBasisConversion:
     def test_explicit_on_hadamard(self):
         s2 = np.sqrt(0.5)
         ptm_hadamard_0xy1_should_be = np.array(
-                [[0.5, s2, 0, 0.5],
-                 [s2, 0, 0, -s2],
-                 [0,0,-1, 0],
-                 [0.5, -s2, 0, 0.5]]
-                )
+            [[0.5, s2, 0, 0.5],
+             [s2, 0, 0, -s2],
+             [0, 0, -1, 0],
+             [0.5, -s2, 0, 0.5]]
+        )
 
         ptm3x3hadamard = np.array([[0, 0, 1], [0, -1, 0], [1, 0, 0]])
         ptm_hadamard = ptm.to_0xy1_basis(ptm3x3hadamard)
@@ -37,12 +43,13 @@ class TestPTMBasisConversion:
         ptm_hadamard = ptm.to_0xy1_basis(ptm4x3hadamard)
         assert np.allclose(ptm_hadamard_0xy1_should_be, ptm_hadamard)
 
-        ptm4x4hadamard = np.array([[1, 0, 0, 0], [0, 0, 0, 1], [0, 0, -1, 0], [0, 1, 0, 0]])
+        ptm4x4hadamard = np.array(
+            [[1, 0, 0, 0], [0, 0, 0, 1], [0, 0, -1, 0], [0, 1, 0, 0]])
         ptm_hadamard = ptm.to_0xy1_basis(ptm4x4hadamard)
         assert np.allclose(ptm_hadamard_0xy1_should_be, ptm_hadamard)
 
     def test_inversion(self):
-        p = np.random.random((4,4))
+        p = np.random.random((4, 4))
 
         p[0, :] = np.array([1, 0, 0, 0])
 
@@ -50,26 +57,27 @@ class TestPTMBasisConversion:
 
         assert np.allclose(p, ptm.to_0xyz_basis(ptm.to_0xy1_basis(p)))
 
+
 class TestRotations:
     def test_xyz(self):
-        ptm_x = ptm.rotate_x_ptm(np.pi/2)
-        ptm_z = ptm.rotate_x_ptm(np.pi/2)
-        ptm_y = ptm.rotate_x_ptm(-np.pi/2)
+        ptm_x = ptm.rotate_x_ptm(np.pi / 2)
+        ptm_z = ptm.rotate_x_ptm(np.pi / 2)
+        ptm_y = ptm.rotate_x_ptm(-np.pi / 2)
 
         ptm_xyz = np.dot(ptm_y, np.dot(ptm_z, ptm_x))
 
         assert np.allclose(ptm_xyz, ptm_z)
 
     def test_power(self):
-        ptm_x = ptm.rotate_x_ptm(2*np.pi/7)
+        ptm_x = ptm.rotate_x_ptm(2 * np.pi / 7)
         ptm_x7 = np.linalg.matrix_power(ptm_x, 7)
         assert np.allclose(ptm_x7, np.eye(4))
 
-        ptm_y = ptm.rotate_y_ptm(2*np.pi/7)
+        ptm_y = ptm.rotate_y_ptm(2 * np.pi / 7)
         ptm_y7 = np.linalg.matrix_power(ptm_y, 7)
         assert np.allclose(ptm_y7, np.eye(4))
 
-        ptm_z = ptm.rotate_z_ptm(2*np.pi/7)
+        ptm_z = ptm.rotate_z_ptm(2 * np.pi / 7)
         ptm_z7 = np.linalg.matrix_power(ptm_z, 7)
         assert np.allclose(ptm_z7, np.eye(4))
 
@@ -90,14 +98,15 @@ class TestRotations:
         assert np.allclose(state, ground_state)
 
     def test_xyx_ground_state(self):
-        
+
         state = ground_state
 
-        state = ptm.rotate_x_ptm(np.pi/2).dot(state)
-        state = ptm.rotate_y_ptm(np.pi/2).dot(state)
-        state = ptm.rotate_x_ptm(-np.pi/2).dot(state)
+        state = ptm.rotate_x_ptm(np.pi / 2).dot(state)
+        state = ptm.rotate_y_ptm(np.pi / 2).dot(state)
+        state = ptm.rotate_x_ptm(-np.pi / 2).dot(state)
 
         assert np.allclose(state, ground_state)
+
 
 class TestAmpPhaseDamping:
     def test_does_nothing_to_ground_state(self):
@@ -111,13 +120,13 @@ class TestAmpPhaseDamping:
 
         assert np.allclose(p.dot(r), ground_state)
 
+
 class TestGenAmpDamping:
     def test_equal_to_amp_damping(self):
         p1 = ptm.amp_ph_damping_ptm(gamma=0.42, lamda=0)
         p2 = ptm.gen_amp_damping_ptm(gamma_down=0.42, gamma_up=0)
-        
-        assert np.allclose(p1, p2)
 
+        assert np.allclose(p1, p2)
 
     def test_strong_exciting_gives_excited_state(self):
         r = random_state()
@@ -126,13 +135,10 @@ class TestGenAmpDamping:
         assert np.allclose(p.dot(r), excited_state)
 
 
-
-
 class TestKrausToPTM:
-    
     def test_multiplicative_one_qubit(self):
-        a = np.random.random((2,2))
-        b = np.random.random((2,2))
+        a = np.random.random((2, 2))
+        b = np.random.random((2, 2))
 
         ptm_a = ptm.single_kraus_to_ptm(a)
         ptm_b = ptm.single_kraus_to_ptm(b)
@@ -140,10 +146,176 @@ class TestKrausToPTM:
         assert np.allclose(ptm_ab, np.matmul(ptm_a, ptm_b))
 
     def test_multiplicative_two_qubit(self):
-        a = np.random.random((4,4))
-        b = np.random.random((4,4))
+        a = np.random.random((4, 4))
+        b = np.random.random((4, 4))
 
         ptm_a = ptm.double_kraus_to_ptm(a)
         ptm_b = ptm.double_kraus_to_ptm(b)
         ptm_ab = ptm.double_kraus_to_ptm(np.matmul(a, b))
         assert np.allclose(ptm_ab, np.matmul(ptm_a, ptm_b))
+
+
+class TestPauliBasis:
+    def test_basic(self):
+
+        pb = ptm.PauliBasis_0xy1()
+
+        assert len(pb.computational_basis_vectors) == 2
+        assert pb.dim_hilbert == 2
+        assert pb.dim_pauli == 4
+
+        pb = ptm.PauliBasis_exyz()
+
+        assert len(pb.computational_basis_vectors) == 2
+        assert pb.dim_hilbert == 2
+        assert pb.dim_pauli == 4
+
+        pb = ptm.GeneralBasis(2)
+
+        assert len(pb.computational_basis_vectors) == 2
+        assert pb.dim_hilbert == 2
+        assert pb.dim_pauli == 4
+
+        pb = ptm.GeneralBasis(3)
+
+        assert len(pb.computational_basis_vectors) == 3
+        assert pb.dim_hilbert == 3
+        assert pb.dim_pauli == 9
+
+
+some_pauli_bases = [
+    ptm.PauliBasis_0xy1(),
+    ptm.PauliBasis_exyz(),
+    ptm.GeneralBasis(2),
+    ptm.GeneralBasis(3)
+]
+
+
+class TestPTMs:
+
+    @pytest.mark.parametrize("pb", some_pauli_bases)
+    def test_unit_matrix_conjunction(self, pb):
+
+        e_hilbert = np.eye(pb.dim_hilbert)
+
+        unit_ptm = ptm.ConjunctionPTM(e_hilbert)
+
+        e_pauli = unit_ptm.get_matrix(pb)
+
+        assert e_pauli == pytest.approx(np.eye(pb.dim_pauli))
+
+    @pytest.mark.parametrize("pb", some_pauli_bases)
+    def test_unit_matrix_lincomb(self, pb):
+
+        e_hilbert = np.eye(pb.dim_hilbert)
+
+        unit_ptm = ptm.ConjunctionPTM(e_hilbert)
+
+        unit_ptm2 = 0.5 * unit_ptm + 0.5 * unit_ptm
+
+        e_pauli = unit_ptm2.get_matrix(pb)
+        assert e_pauli == pytest.approx(np.eye(pb.dim_pauli))
+        assert len(unit_ptm2.elements) == 1
+
+        unit_ptm2 *= -1
+        unit_ptm2 -= 2 * unit_ptm2
+
+        e_pauli = unit_ptm2.get_matrix(pb)
+        assert e_pauli == pytest.approx(np.eye(pb.dim_pauli))
+        assert len(unit_ptm2.elements) == 1
+
+        unit_ptm2 = unit_ptm2 * 0.5 + 0.5 * unit_ptm
+
+        e_pauli = unit_ptm2.get_matrix(pb)
+        assert e_pauli == pytest.approx(np.eye(pb.dim_pauli))
+        assert len(unit_ptm2.elements) == 1
+
+    @pytest.mark.parametrize("pb", some_pauli_bases)
+    def test_unit_product(self, pb):
+
+        unit_ptm = ptm.ProductPTM([])
+        e_pauli = unit_ptm.get_matrix(pb)
+        assert e_pauli == pytest.approx(np.eye(pb.dim_pauli))
+
+    def test_0xy1_basis_explicit_on_hadamard(self):
+        s2 = np.sqrt(0.5)
+        ptm_hadamard_0xy1_should_be = np.array(
+            [[0.5, s2, 0, 0.5],
+             [s2, 0, 0, -s2],
+             [0, 0, -1, 0],
+             [0.5, -s2, 0, 0.5]]
+        )
+
+        pb = ptm.PauliBasis_0xy1()
+        hadamard = ptm.ConjunctionPTM([[s2, s2], [s2, -s2]])
+
+        ptm_hadamard = hadamard.get_matrix(pb)
+        assert np.allclose(ptm_hadamard_0xy1_should_be, ptm_hadamard)
+
+
+some_2d_pauli_bases = [
+    ptm.PauliBasis_0xy1(),
+    ptm.PauliBasis_exyz(),
+    ptm.GeneralBasis(2),
+]
+
+
+class TestRotationsPTM:
+    @pytest.mark.parametrize("pb", some_2d_pauli_bases)
+    def test_xyz(self, pb):
+        ptm_x = ptm.RotateXPTM(np.pi / 2)
+        ptm_z = ptm.RotateXPTM(np.pi / 2)
+        ptm_y = ptm.RotateXPTM(-np.pi / 2)
+
+        # explicit construction
+        ptm_xyz = ptm.ProductPTM([ptm_x, ptm_y, ptm_z])
+        assert len(ptm_xyz.elements) == 3
+
+        # implicit construction
+        ptm_xyz2 = ptm_x @ ptm_y @ ptm_z
+        assert len(ptm_xyz2.elements) == 3
+
+        mat_xyz = ptm_xyz.get_matrix(pb)
+        mat_xyz2 = ptm_xyz2.get_matrix(pb)
+        mat_z = ptm_xyz.get_matrix(pb)
+
+        assert np.allclose(mat_xyz, mat_z)
+        assert np.allclose(mat_xyz2, mat_z)
+
+    @pytest.mark.parametrize("pb", some_2d_pauli_bases)
+    def test_power(self, pb):
+        for Rot in [ptm.RotateXPTM, ptm.RotateYPTM, ptm.RotateZPTM]:
+            ptm_x = Rot(2 * np.pi / 7)
+            ptm_x7 = ptm.ProductPTM([ptm_x]*7)
+            mat_x7 = ptm_x7.get_matrix(pb)
+            assert np.allclose(mat_x7, np.eye(4))
+
+    @pytest.mark.parametrize("pb", some_2d_pauli_bases)
+    def test_excite_deexcite_ground_state(self, pb):
+
+        ptm_x = ptm.RotateXPTM(np.pi).get_matrix(pb)
+        ptm_y = ptm.RotateYPTM(np.pi).get_matrix(pb)
+        ptm_z = ptm.RotateZPTM(np.pi).get_matrix(pb)
+
+        ground_state = pb.computational_basis_vectors[0]
+        state = ground_state
+        state = np.dot(ptm_x, state)
+        state = np.dot(ptm_y, state)
+        state = np.dot(ptm_z, state)
+        state = np.dot(ptm_x, state)
+        state = np.dot(ptm_y, state)
+        state = np.dot(ptm_z, state)
+
+        assert np.allclose(state, ground_state)
+
+    @pytest.mark.parametrize("pb", some_2d_pauli_bases)
+    def test_xyx_ground_state(self, pb):
+
+        ground_state = pb.computational_basis_vectors[0]
+        state = ground_state
+
+        state = ptm.RotateXPTM(np.pi/2).get_matrix(pb) @ state
+        state = ptm.RotateYPTM(np.pi/2).get_matrix(pb) @ state
+        state = ptm.RotateXPTM(-np.pi/2).get_matrix(pb) @ state
+
+        assert np.allclose(state, ground_state)
