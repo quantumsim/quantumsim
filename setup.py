f9a4433741d4614163bbd819ccee059c5a995d1b
Viacheslav Ostroukh <V.Ostroukh@tudelft.nl>
2018-10-02 08:40:30 +0200

    refactor requirements to separate file

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='qs2',
-    version='0.1',
+    version='1.0.0-dev0',
     description=(
         'Simulation of quantum circuits under somewhat realistic condititons'
     ),
-    author='Brian Tarasinski et al',
+    author='Quantumsim Authors',
     author_email='brianzi@physik.fu-berlin.de',
     packages=find_packages('.'),
     ext_package='qs2',
@@ -17,9 +17,5 @@ setup(
         # all Cuda and json files we can find
         '': ['*.cu', '*.json'],
     },
-    install_requires=[
-        "numpy>=1.12",
-        "pytest",
-        "matplotlib",
-    ]
+    install_requires=list(open('requirements.txt').read().strip().split('\n')),
 )
