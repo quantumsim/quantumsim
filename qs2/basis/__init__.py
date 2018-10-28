27f8749801ab6349dd5f83c7e3ccfddeac6babad
Thomas OBrien <obrien@lorentz.leidenuniv.nl>
2018-10-28 12:53:19 +0100

    Started setup folder, made imports in basis/__init__.py explicit

--- qs2/basis/__init__.py
+++ qs2/basis/__init__.py
@@ -1,2 +1,30 @@
-from .pauli import PauliBasis
-from .basis import *
+from .basis import (
+    gell_mann,
+    general,
+    twolevel_0xy1,
+    twolevel_ixyz)
+
+from compilers import(
+    CompilerBlock,
+    TwoPTMCompiler)
+
+from .pauli import (
+    AdjunctionPLM,
+    AmplitudePhaseDampingPTM,
+    ConjunctionPTM,
+    CPhaseRotationPTM,
+    ExplicitBasisPTM,
+    LindbladPLM,
+    LinearCombPTM,
+    PauliBasis,
+    PLMIntegrator,
+    ProductPTM,
+    PTM,
+    RotateXPTM,
+    RotateYPTM,
+    RotateZPTM,
+    TwoKrausPTM,
+    TwoPTM,
+    TwoPTMProduct,
+    TwoPTMExplicit)
+
