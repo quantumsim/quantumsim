b9d55fe3d2207b67f585500ccba7d4c9ae2b31ed
Thomas OBrien <obrien@lorentz.leidenuniv.nl>
2018-10-28 13:48:22 +0100

    Wrote test, found bug, fixed bug.

---
+++ qs2/test/test_basis.py
@@ -0,0 +1,24 @@
+# This file is part of quantumsim. (https://gitlab.com/quantumsim/quantumsim)
+# (c) 2018 Quantumsim Authors
+# Distributed under the GNU GPLv3. See LICENSE.txt or
+# https://www.gnu.org/licenses/gpl.txt
+
+import numpy as np
+
+from qs2.basis import (
+    gell_mann,
+    general,
+    PauliBasis,
+    twolevel_0xy1,
+    twolevel_ixyz)
+
+
+class TestBasis:
+
+    def test_inits(self):
+        b0 = gell_mann(2)
+        b1 = general(2)
+        assert len(b0.labels) == 4
+        assert len(b1.labels) == 4
+        assert np.allclose(b0.vectors.shape,np.array([4,2,2]))
+        assert np.allclose(b1.vectors.shape,np.array([4,2,2]))
