2ffa04e13e4a66c6cf5f0432571298e9a93236b5
Viacheslav Ostroukh <V.Ostroukh@tudelft.nl>
2018-11-13 15:25:26 +0100

    utilize miniver for versioning info

---
+++ qs2/__init__.py
@@ -0,0 +1,15 @@
+# noinspection PyUnresolvedReferences,PyProtectedMember
+from ._version import __version__
+
+__all__ = []
+
+for module in ['basis', 'compilers', 'gates', 'operators', 'setup', 'system']:
+    exec('from . import {0}'.format(module))
+    __all__.append(module)
+
+
+def test(verbose=True):
+    from pytest import main
+    from os.path import dirname, abspath, join
+    return main([dirname(abspath(join(__file__, '..'))), "-s"] +
+                (['-v'] if verbose else []))
