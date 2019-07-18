de5be7df204604d77098de8b5a0a6db06f04041a
Thomas OBrien <obrien@lorentz.leidenuniv.nl>
2019-07-18 18:21:47 +0200

    Name consistency change

--- quantumsim/models/depolarizing.py
+++ quantumsim/models/depolarizing.py
@@ -8,24 +8,24 @@ from quantumsim.models import noiseless
 import numpy as np
 
 
-def depolarizing_channel(depol_prob):
+def depolarizing_channel(depol_noise):
     op = np.identity(4)
     for j in range(3):
-        op[j+1, j+1] *= (1-depol_prob)
+        op[j+1, j+1] *= (1-depol_noise)
     op = ptm.to_0xy1_basis(op)
     return op
 
 
 class DepolarizingSinglePTMGate(SinglePTMGate):
-    def __init__(self, bit, time, depol_prob, **kwargs):
+    def __init__(self, bit, time, depol_noise, **kwargs):
         super().__init__(bit, time, **kwargs)
-        error_ptm = depolarizing_channel(depol_prob)
+        error_ptm = depolarizing_channel(depol_noise)
         self.ptm = error_ptm @ self.ptm
 
 class DepolarizingTwoPTMGate(TwoPTMGate):
-    def __init__(self, bit0, bit1, time, depol_prob, **kwargs):
+    def __init__(self, bit0, bit1, time, depol_noise, **kwargs):
         super().__init__(bit0, bit1, time, **kwargs)
-        error_ptm = depolarizing_channel(depol_prob)
+        error_ptm = depolarizing_channel(depol_noise)
         error_ptm = np.kron(error_ptm, error_ptm)
         self.two_ptm = error_ptm @ self.two_ptm
 
@@ -35,7 +35,7 @@ class RotateY(DepolarizingSinglePTMGate, noiseless.RotateY):
     pass
 class RotateZ(DepolarizingSinglePTMGate, noiseless.RotateZ):
     pass
-class CPhase(DepolarizingSinglePTMGate, noiseless.CPhase):
+class CPhase(DepolarizingTwoPTMGate, noiseless.CPhase):
     pass
 class CNOT(DepolarizingTwoPTMGate, noiseless.CNOT):
     pass
