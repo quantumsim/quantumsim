6ed0150043f9480e91ac707daa3680f3379bddc2
Viacheslav Ostroukh <V.Ostroukh@tudelft.nl>
2019-07-24 16:31:24 +0200

    implement setup loading

--- quantumsim/setups/setup.py
+++ quantumsim/setups/setup.py
@@ -1,4 +1,11 @@
 import yaml
+from collections import defaultdict
+
+from copy import copy
+
+
+class SetupLoadError(RuntimeError):
+    pass
 
 
 class Setup:
@@ -11,9 +18,57 @@ class Setup:
     """
     def __init__(self, setup):
         if isinstance(setup, dict):
-            self._setup_dict = setup
+            setup_dict = setup
         else:
-            self._setup_dict = yaml.safe_load(setup)
+            setup_dict = yaml.safe_load(setup)
+
+        self._qubits = {}
+        self._gates = defaultdict(dict)
+
+        version = setup_dict.get('version', '1')
+        if version != '1' and version != 1:
+            raise SetupLoadError('Unknown setup schema version: {}'
+                                 .format(version))
+        self._load_setup_v1(setup_dict)
+
+    def _load_setup_v1(self, setup_dict):
+        qubits = copy(setup_dict.get('qubits'))
+        if qubits is None:
+            raise SetupLoadError('Setup does not define "qubits" section')
+
+        for qubit_dict in qubits:
+            name = str(qubit_dict.pop('name', ''))
+            if name in self._qubits.keys():
+                what = 'Parameters for qubit "{}"'.format(name) if name else \
+                    'Default qubit parameters'
+                raise SetupLoadError(what + ' defined repeatedly in the setup.')
+            self._qubits[name] = qubit_dict
+
+        gates = copy(setup_dict.get('gates'))
+        if gates is None:
+            raise SetupLoadError('Setup does not define "gates" section')
+
+        for gate_dict in gates:
+            name = gate_dict.pop('name', None)
+            if not name:
+                raise SetupLoadError('All gate declarations in the setup must '
+                                     'include a field "name".')
+            qubits = gate_dict.pop('qubits', [])
+            if isinstance(qubits, list):
+                qubits = tuple(qubits)
+            elif isinstance(qubits, str):
+                qubits = (qubits,)
+            else:
+                raise SetupLoadError('"qubits" keyword in a gate definition '
+                                     'must be either a string, or a list.')
+            if qubits in self._gates[name].keys():
+                what = ('Default parameters for gate "{}"'.format(name)
+                        if len(qubits) == 0 else
+                        'Parameters for gate "{}" on qubits {}'
+                        .format(name, ", ".join(qubits)))
+                raise SetupLoadError(what +
+                                     ' are defined repeatedly in the setup.')
+            self._gates[name][qubits] = gate_dict
 
     @classmethod
     def from_file(cls, filename):
--- test/test_setup.py
+++ test/test_setup.py
@@ -1,5 +1,9 @@
 import os
+
+import pytest
+
 from quantumsim import Setup
+from quantumsim.setups.setup import SetupLoadError
 
 test_dir = os.path.dirname(os.path.abspath(__file__))
 
@@ -8,3 +12,80 @@ class TestSetup:
     def test_setup_load(self):
         _ = Setup.from_file(os.path.join(test_dir,
                             'time_aware_setup_with_defaults.yaml'))
+
+        with pytest.raises(SetupLoadError,
+                           match='Unknown setup schema version: 10'):
+            Setup("version: 10")
+        with pytest.raises(SetupLoadError,
+                           match='Setup does not define "qubits" section'):
+            Setup("gates: []")
+        with pytest.raises(SetupLoadError,
+                           match='Setup does not define "gates" section'):
+            Setup("qubits: []")
+        with pytest.raises(SetupLoadError, match="Default qubit parameters "
+                                                 "defined repeatedly .*"):
+            Setup("""
+            qubits:
+            - t1: 100
+            - t1: 200
+            gates: []
+            """)
+        with pytest.raises(SetupLoadError, match='Parameters for qubit "X" '
+                                                 'defined repeatedly .*'):
+            Setup("""
+            qubits:
+            - t1: 500
+            - name: X
+              t1: 100
+            - name: X
+              t1: 200
+            gates: []
+            """)
+        with pytest.raises(SetupLoadError, match='.* include a field "name"'):
+            Setup("""
+            qubits: []
+            gates:
+            - param_name: value
+            """)
+        with pytest.raises(SetupLoadError,
+                           match='.* either a string, or a list'):
+            Setup("""
+            qubits: []
+            gates:
+            - name: X
+              qubits: 1
+            """)
+        Setup("""
+            qubits: []
+            gates:
+            - name: X
+              param_name: 1
+            - name: X
+              qubits: QUBIT
+              param_name: 2
+            """)
+        with pytest.raises(SetupLoadError,
+                           match='Default parameters for gate "X" are defined '
+                                 'repeatedly in the setup'):
+            Setup("""
+            qubits: []
+            gates:
+            - name: X
+              param_name: 1
+            - name: X
+              param_name: 2
+            """)
+        with pytest.raises(SetupLoadError,
+                           match='Parameters for gate "X" on qubits QUBIT are '
+                                 'defined repeatedly in the setup'):
+            Setup("""
+            qubits: []
+            gates:
+            - name: X
+              qubits: QUBIT
+              param_name: 1
+            - name: X
+              qubits: QUBIT
+              param_name: 2
+            """)
+
