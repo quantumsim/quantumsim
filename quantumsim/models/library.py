0067616164cb9ca7fc28f26aa5caf367521eadaa
Boris Varbanov <b.m.varbanov@gmail.com>
2020-03-13 12:59:33 +0100

    Add docstring to the ideal model

--- quantumsim/models/library.py
+++ quantumsim/models/library.py
@@ -5,24 +5,41 @@ from ..operations.operation import ParametrizedOperation
 from ..operations.qubits import (
     cnot, cphase, swap, hadamard, rotate_x, rotate_y, rotate_z)
 from .model import Model
+from ..setups import Setup
 
 _basis = bases.general(2)
 _basis_classical = bases.general(2).subbasis([0, 1]),
 
 
 def get_ideal_setup(qubits):
+    """
+    Initializes an ideal setup file for a given list of qubit.
+
+    Parameters
+    ----------
+    qubits : list or tuple
+        The list of the qubits involved in the circuit.
+
+    Returns
+    -------
+    quantumsim.Setup
+        The setup file containing the ideal gate and qubit parameters.
+    """
     _gate_setup = [{
         'time_1qubit': 0,
         'time_2qubit': 0,
         'time_measure': 0}]
     _qubit_setup = [{'qubit': q} for q in qubits]
-    return {
+    return Setup({
         'version': '1',
         'name': 'Ideal Setup',
-        'setup': _gate_setup + _qubit_setup}
+        'setup': _gate_setup + _qubit_setup})
 
 
 class IdealModel(Model):
+    """
+    A model for an ideal error model, where gates are instantaneous and perfect, while the qubits experiences no noise.
+    """
     _ptm_project = [rotate_x(0).set_bases(
         (bases.general(2).subbasis([i]),), (bases.general(2).subbasis([i]),)
     ) for i in range(2)]
@@ -32,7 +49,7 @@ class IdealModel(Model):
     @Model.gate(duration='time_1qubit', plot_metadata={
         'style': 'box', 'label': '$X({theta})$'})
     def rotate_x(self, qubit):
-        """Rotation around the X-axis Parameters: `angle` (degrees).
+        """Rotation around the X-axis by a given angle. Parameters: `angle` (degrees).
         """
         return (ParametrizedOperation(
             lambda angle: rotate_x(np.deg2rad(angle)),
@@ -42,7 +59,7 @@ class IdealModel(Model):
     @Model.gate(duration='time_1qubit', plot_metadata={
         'style': 'box', 'label': '$X({theta})$'})
     def rotate_y(self, qubit):
-        """Rotation around the Y-axis. Parameters: `angle` (degrees).
+        """Rotation around the Y-axis by a given angle. Parameters: `angle` (degrees).
         """
         return (ParametrizedOperation(
             lambda angle: rotate_y(np.deg2rad(angle)),
@@ -52,7 +69,7 @@ class IdealModel(Model):
     @Model.gate(duration='time_1qubit', plot_metadata={
         'style': 'box', 'label': '$X({theta})$'})
     def rotate_z(self, qubit):
-        """Rotation around the Z-axis. Parameters: `angle` (degrees).
+        """Rotation around the Z-axis by a given angle. Parameters: `angle` (degrees).
         """
         return (ParametrizedOperation(
             lambda angle: rotate_z(np.deg2rad(angle)),
@@ -62,7 +79,7 @@ class IdealModel(Model):
     @Model.gate(duration='time_1qubit', plot_metadata={
         'style': 'box', 'label': '$H$'})
     def hadamard(self, qubit):
-        """Rotation around the Z-axis. Parameters: `angle` (degrees).
+        """A Hadamard gate.
         """
         return (hadamard().at(qubit),)
 
@@ -74,7 +91,7 @@ class IdealModel(Model):
         ]
     })
     def cphase(self, control_qubit, target_qubit):
-        """Rotation around the Z-axis. Parameters: `angle` (degrees).
+        """Conditional phase rotation of the target qubit by a given angle, depending on the state of the control qubit. Parameters: `angle` (degrees).
         """
         return (ParametrizedOperation(
             lambda angle: cphase(np.deg2rad(angle)),
@@ -89,7 +106,7 @@ class IdealModel(Model):
         ]
     })
     def cnot(self, control_qubit, target_qubit):
-        """Rotation around the Z-axis. Parameters: `angle` (degrees).
+        """Conditional NOT on the target qubit depending on the state of the control qubit. Parameters: `angle` (degrees).
         """
         return (cnot().at(control_qubit, target_qubit),)
 
@@ -101,13 +118,15 @@ class IdealModel(Model):
         ]
     })
     def swap(self, control_qubit, target_qubit):
-        """Rotation around the Z-axis. Parameters: `angle` (degrees).
+        """A SWAP gate.
         """
         return (swap().at(control_qubit, target_qubit),)
 
     @Model.gate(duration='time_measure', plot_metadata={
         'style': 'box', 'label': r'$\circ\!\!\!\!\!\!\!\nearrow$'})
     def measure(self, qubit):
+        """A measurement gate.
+        """
         def project(result):
             if result in (0, 1):
                 return self._ptm_project[result]
