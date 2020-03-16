d7444e6c8536226cbfb08eebab4572da8d319fef
Boris Varbanov <b.m.varbanov@gmail.com>
2020-03-16 02:05:59 +0100

    Remove qutip from state plotter and add plot sugar

--- quantumsim/states/plotter.py
+++ quantumsim/states/plotter.py
@@ -2,7 +2,7 @@ import numpy as np
 from itertools import product
 
 
-def plot(state, ax=None, truncate_levels=None):
+def plot(state, ax=None, truncate_levels=None, colorbar=True, amp_limits=None, phase_limits=None):
     """
 
     Parameters
@@ -22,9 +22,8 @@ def plot(state, ax=None, truncate_levels=None):
     fig : matplotlib.figure.Figure or None
     """
     import matplotlib.pyplot as plt
-    # TODO: get rid of qutip, no sence to carry such a huge dependency for
-    #  such a small task
-    import qutip
+    from matplotlib.colors import Normalize
+    from matplotlib import colorbar
 
     if ax is None:
         fig, ax = plt.subplots(subplot_kw=dict(projection='3d'))
@@ -33,29 +32,73 @@ def plot(state, ax=None, truncate_levels=None):
 
     n_qubits = len(state.qubits)
     pv = state.pauli_vector
-    rho = pv.to_dm()
-    rho /= np.trace(rho)
+    _rho = pv.to_dm()
+    _rho /= np.trace(_rho)
 
     if truncate_levels is not None:
         # Tomo emulation: truncate leaked states and add
-        rho2 = (rho.reshape(state.pauli_vector.dim_hilbert * 2)
-                [(slice(0, truncate_levels),) * (2 * n_qubits)]
-                .reshape(truncate_levels ** n_qubits,
-                         truncate_levels ** n_qubits))
-        trace = np.trace(rho2)
-        rho2 += ((1 - trace) * np.identity(2**n_qubits) *
-                 truncate_levels ** -n_qubits)
-        assert np.allclose(np.trace(rho2), 1)
+        rho = (_rho.reshape(state.pauli_vector.dim_hilbert * 2)
+               [(slice(0, truncate_levels),) * (2 * n_qubits)]
+               .reshape(truncate_levels ** n_qubits,
+                        truncate_levels ** n_qubits))
+        trace = np.trace(rho)
+        rho += ((1 - trace) * np.identity(2**n_qubits) *
+                truncate_levels ** -n_qubits)
+        assert np.allclose(np.trace(rho), 1)
         dim = [truncate_levels] * n_qubits
     else:
-        rho2 = rho
+        rho = _rho
         dim = pv.dim_hilbert
 
     def tuple_to_string(tup):
-        return "".join(str(x) for x in tup)
+        state = ''.join(str(x) for x in tup)
+        return r'$\left| %s \right\rangle$' % state
 
     labels = [tuple_to_string(x) for x in product(*(range(d) for d in dim))]
-    rho_q = qutip.Qobj(rho2, dims=[dim, dim])
-    qutip.matrix_histogram_complex(rho_q, ax=ax, xlabels=labels, ylabels=labels)
+
+    if phase_limits and isinstance(phase_limits, (list, tuple)):
+        assert len(phase_limits) == 2
+    else:
+        phase_limits = (-np.pi, np.pi)
+
+    norm = Normalize(*phase_limits)
+    cmap = plt.get_cmap('plasma')
+    colors = cmap(norm(np.angle(rho.flatten())))
+
+    if amp_limits and isinstance(phase_limits, (list, tuple)):
+        assert len(amp_limits) == 2
+    else:
+        amp_limits = (0, 1)
+
+    xpos, ypos = np.meshgrid(*(range(dim) for dim in rho.shape))
+    xpos = xpos.flatten()
+    ypos = ypos.flatten()
+    zpos = 0
+    dx = dy = 0.5 * np.ones(rho.size)
+    dz = np.real(rho.flatten())
+
+    ax.bar3d(xpos, ypos, zpos, dx, dy, dz, color=colors)
+    ax.set_zlim3d(amp_limits)
+
+    ax.axes.w_xaxis.set_major_locator(plt.IndexLocator(1, 0.25))
+    ax.set_xticklabels(labels)
+
+    ax.axes.w_yaxis.set_major_locator(plt.IndexLocator(1, 0.25))
+    ax.set_yticklabels(labels)
+
+    plt.setp(ax.get_xticklabels(), rotation=45, ha="center", va='baseline',
+             rotation_mode="anchor")
+
+    plt.setp(ax.get_yticklabels(), rotation=-45, ha="center", va='baseline',
+             rotation_mode="anchor")
+
+    ax.set_zlabel('Amplitude')
+
+    if colorbar:
+        cax, _ = colorbar.make_axes(ax)
+        cb = colorbar.ColorbarBase(cax, cmap=cmap, norm=norm)
+        cb.set_ticks((-np.pi, 0, np.pi))
+        cb.set_ticklabels((r'$-\pi$', r'$0$', r'$\pi$'))
+        cb.set_label('Phase')
 
     return fig
