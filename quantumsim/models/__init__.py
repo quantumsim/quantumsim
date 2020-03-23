b9196ef2f504def1fa6ead8d321c5119ff774e88
Boris Varbanov <b.m.varbanov@gmail.com>
2020-03-23 18:27:56 +0100

    add transmon model to init

--- quantumsim/models/__init__.py
+++ quantumsim/models/__init__.py
@@ -1,4 +1,4 @@
 from .model import Model, WaitPlaceholder
-from .library import IdealModel, get_ideal_setup
+from .library import IdealModel, TransmonModel, get_ideal_setup, get_transmon_setup
 
 __all__ = ['Model', 'WaitPlaceholder', 'IdealModel', 'get_ideal_setup']
