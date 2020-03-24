703400aaf0e6b5a78cdb84fa8f1cdf4a9af955a0
Boris Varbanov <b.m.varbanov@gmail.com>
2020-03-24 02:55:43 +0100

    Current progress on controller

--- quantumsim/controllers/controller.py
+++ quantumsim/controllers/controller.py
@@ -1,20 +1,52 @@
+from itertools import chain
+
 import xarray as xr
 
-from ..circuits import FinalizedCircuit
+from ..circuits import TimeAwareCircuit, TimeAgnosticCircuit
 from ..states import State
 
 
 class Controller:
-    def __init__(self, state):
+    # TODO: Add state initialization methods
+
+    def __init__(self, state, circuits, circuit_params):
+        if not isinstance(state, State):
+            raise ValueError("Please provide an initial state")
         self._state = state
+
+        if not isinstance(circuits, dict):
+            raise ValueError("circuits should be a dictionary")
+        if not all(isinstance(circ_name, str) for circ_name in circuits.keys()):
+            raise ValueError("Only names")
+        if not all(isinstance(circ, (TimeAwareCircuit, TimeAgnosticCircuit))
+                   for circ in circuits.values()):
+            raise ValueError("Only circuits")
+
+        _free_circ_params = list(
+            circuit.free_parameters for circuit in circuits.values())
+
+        # Common pareters in different circuits currently not allowed
+        common_params = set.intersection(*_free_circ_params)
+        if len(common_params) > 0:
+            # TODO: add a better message ;D
+            raise RuntimeError("ERROR")
+
+        self._params = dict.fromkeys(chain(*_free_circ_params), None)
+
+        self._circuits = {circ_name: circ.finalize()
+                          for circ_name, circ in circuits.items()}
 
     @property
     def state(self):
         return self._state
 
-    def run_round(self, num_rounds=1):
+    def run_round(self):
         raise NotImplementedError
 
     def _apply_circuit(self, circuit_name, **kwargs):
+        try:
+            self._circuits[circuit_name] @ self._state
+        except KeyError:
+            raise KeyError("Message")
         outcome = xr.DataArray()
         return outcome
