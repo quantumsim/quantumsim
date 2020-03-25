8e9a8be124ebeb90e3fd6183979e24e9e020f99e
Boris Varbanov <b.m.varbanov@gmail.com>
2020-03-25 13:32:20 +0100

    Index deparameterize function in subpackage

--- quantumsim/circuits/__init__.py
+++ quantumsim/circuits/__init__.py
@@ -1,7 +1,8 @@
 from .circuit import TimeAgnosticGate, TimeAgnosticCircuit,\
-    TimeAwareGate, TimeAwareCircuit, FinalizedCircuit, allow_param_repeat
+    TimeAwareGate, TimeAwareCircuit, FinalizedCircuit, allow_param_repeat, deparametrize
 from .plotter import plot
 
 __all__ = ['TimeAgnosticGate', 'TimeAgnosticCircuit', 'TimeAwareGate',
            'TimeAwareCircuit', 'FinalizedCircuit', 'allow_param_repeat',
+           'deparametrize',
            'plot']
