cfae4502fd35e075dfa99e3525ad4299ec12af40
Boris Varbanov <b.m.varbanov@gmail.com>
2020-04-17 22:31:00 +0200

    Some improvements to the operation plotters

--- quantumsim/operations/plotter.py
+++ quantumsim/operations/plotter.py
@@ -23,24 +23,27 @@ def plot(operation, bases_in=None, bases_out=None, ax=None, truncate_levels=None
     from matplotlib import colorbar
 
     if ax is None:
-        fig, ax = plt.subplots()
+        fig, ax = plt.subplots(constrained_layout=True)
     else:
         fig = None
 
     dim = operation.dim_hilbert
-    num_pauli_elems = (dim**2) * operation.num_qubits
+    num_qubits = operation.num_qubits
+    num_basis_elements = (dim ** 2) ** num_qubits
 
     _bases_in = bases_in or operation.bases_in
     _bases_out = bases_out or operation.bases_out
 
-    ptm = operation.ptm(_bases_in, _bases_out)
+    ptm = operation.ptm(_bases_in, _bases_out).reshape(
+        num_basis_elements, num_basis_elements
+    )
 
     if truncate_levels is not None:
         raise NotImplementedError
 
     def tuple_to_string(tup):
-        pauli_element = ''.join(str(x) for x in tup)
-        return r'$%s$' % pauli_element
+        pauli_element = "".join(str(x) for x in tup)
+        return r"$%s$" % pauli_element
 
     _bases_in_labels = (basis.labels for basis in _bases_in)
     x_labels = [tuple_to_string(x) for x in product(*_bases_in_labels)]
@@ -48,20 +51,21 @@ def plot(operation, bases_in=None, bases_out=None, ax=None, truncate_levels=None
     _bases_out_labels = (basis.labels for basis in _bases_out)
     y_labels = [tuple_to_string(x) for x in product(*_bases_out_labels)]
 
-    img = ax.imshow(ptm, cmap='bwr')
+    img = ax.imshow(ptm, cmap="bwr", aspect="equal", origin="upper")
+    img.set_clim(vmin=-1, vmax=1)
 
-    ax.set_xticks(range(num_pauli_elems))
+    ax.set_xticks(range(num_basis_elements))
     ax.set_xticklabels(x_labels)
-    ax.set_xlabel('Input basis')
+    ax.set_xlabel("Input basis")
 
-    ax.set_yticks(range(num_pauli_elems))
+    ax.set_yticks(range(num_basis_elements))
     ax.set_yticklabels(y_labels)
-    ax.set_xlabel('Output basis')
+    ax.set_xlabel("Output basis")
 
     if colorbar:
         cax, _ = colorbar.make_axes(ax)
         cbar = colorbar.Colorbar(cax, img)
         cbar.set_ticks((-1, 0, 1))
-        cbar.ax.set_ylabel('Amplitude', rotation=270)
+        cbar.ax.set_ylabel("Amplitude", rotation=270)
 
     return fig
