6c49ff2ee48b5bb504d498630ab6bcc80152f806
Boris Varbanov <b.m.varbanov@gmail.com>
2020-04-24 11:50:30 +0200

    Add tests for controller initialization and state preparation

--- test/test_controller.py
+++ test/test_controller.py
@@ -1,17 +1,46 @@
 import pytest
-
-from numpy import pi
+import numpy as np
 import xarray as xr
 from pytest import approx
-from quantumsim import Controller
-from quantumsim import bases
-from quantumsim.circuits import FinalizedCircuit, Circuit
 
-single_qubit_bases = (bases.general(2),)
-two_qubit_bases = single_qubit_bases * 2
+from quantumsim import Controller, gates, State
 
 
 # noinspection PyTypeChecker
 class TestController:
-    def test_input(self):
-        pass
+    def test_controller_init(self):
+        with pytest.raises(ValueError):
+            c = Controller(None)
+        with pytest.raises(ValueError):
+            c = Controller({})
+
+        circ = (gates.rotate_x('Q0') + gates.cnot('Q0', 'Q1'))
+        circ_f = circ.finalize()
+        with pytest.raises(ValueError):
+            c = Controller(circ)
+
+        with pytest.raises(ValueError):
+            c = Controller({None, circ})
+
+        with pytest.raises(ValueError):
+            c = Controller(dict(test=circ))
+
+        c = Controller(dict(test=circ_f))
+        assert c._qubits == set(['Q0', 'Q1'])
+        assert c._parameters == {}
+
+    def test_prepare_state(self):
+        circ = (gates.rotate_x('Q0') + gates.cnot('Q0', 'Q1'))
+        circ_f = circ.finalize()
+        c = Controller(dict(test=circ_f))
+        assert c.state is None
+        c.prepare_state()
+        assert c.state is not None
+        assert isinstance(c.state, State)
+        assert np.allclose(c.state.pauli_vector.to_dm(),
+                           State(["Q0", "Q1"]).pauli_vector.to_dm())
+
+        c = Controller(dict(test=circ_f))
+        c.prepare_state(dim=3)
+        assert np.allclose(c.state.pauli_vector.to_dm(),
+                           State(["Q0", "Q1"], dim=3).pauli_vector.to_dm())
