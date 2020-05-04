0c6b312c97e2a85e5de5b8696ff151121e21d8c3
Boris Varbanov <b.m.varbanov@gmail.com>
2020-05-04 16:52:36 +0200

    Finally get rid of the disease that the channels was

--- quantumsim/models/__init__.py
+++ quantumsim/models/__init__.py
@@ -1,9 +1,8 @@
 from .model import Model, WaitPlaceholder
-from .channel import Channel
 from .library import IdealModel
 
 gates = IdealModel()
 # I think this is questionable. I think the gate wrapper
 # should be reformated to be indepedent and gates should be a module
 
-__all__ = ["Model", "Channel", "WaitPlaceholder", "gates"]
+__all__ = ["Model", "WaitPlaceholder", "gates"]
